80cc6e46ed8912d7eeb5eb075b3f08df84cf5f2c
bkantwerk <b@Circuit.de>
2013-05-23 14:17:46 +0200

    Initial Commit

---
+++ HomeConnect/MainMenuLayer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HouseControl
+{
+    public partial class MainMenuLayer : Form
+    {
+        public MainMenuLayer()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //TODO: Implement Blueprint Loading and switch to Control Mode
+            MessageBox.Show("Es muss ein Grundriss erstellt werden, um die Wohnung zu steuern!");
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            HouseBuildingLayer hbl = new HouseBuildingLayer();
+            hbl.ShowDialog();
+        }
+    }
+}
