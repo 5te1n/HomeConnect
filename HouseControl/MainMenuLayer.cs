ca17738ca421dcba168d529da682c543d08f565e
Andrei Stein <andreistein@hotmail.de>
2013-05-24 13:39:23 +0200

    + added HouseControllLayer + Image Ordner für Buttons etc.

--- HouseControl/MainMenuLayer.cs
+++ HouseControl/MainMenuLayer.cs
@@ -12,16 +12,22 @@ namespace HouseControl
 {
     public partial class MainMenuLayer : Form
     {
+        public HouseControllLayer m_HouseControllLayer;
+        
         public MainMenuLayer()
         {
             InitializeComponent();
+            
+            this.m_HouseControllLayer = new HouseControllLayer();
+       
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             //TODO: Implement Blueprint Loading and switch to Control Mode
-            MessageBox.Show("Es muss ein Grundriss erstellt werden, um die Wohnung zu steuern!");
-            
+            //MessageBox.Show("Es muss ein Grundriss erstellt werden, um die Wohnung zu steuern!");
+
+            m_HouseControllLayer.Show(this);
         }
 
         private void button2_Click(object sender, EventArgs e)
